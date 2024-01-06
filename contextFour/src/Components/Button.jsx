87247a8135cff2a2627574d17b5ca16991d0e3e3
Crave <sarkargaurav59@gmail.com>
2024-01-06 20:28:54 +0530

    set new

--- contextFour/src/Components/Button.jsx
+++ contextFour/src/Components/Button.jsx
@@ -7,19 +7,23 @@ export default function ThemeBtn() {
     const {setLightTheme, setDarkTheme} = useTheme()
     const [check,setCheck] = useState(false)
 
+    const clickButton= (e) =>{
+        e.preventDefault();
+        setCheck((prev)=>!prev)
+    }
+
     useEffect(()=>{
         if (check) {
             setDarkTheme()
         } else {
             setLightTheme()
         }
-    },[check,setDarkTheme,setLightTheme])
-
+    },[check,setDarkTheme,setLightTheme]);
 
     return (
         <label className="relative inline-flex items-center cursor-pointer">
 
-            <button className="border-x-2 border-y-2 border-slate-900 p-2" onClick={()=>setCheck((prev) => !prev)}>Click</button>
+            <button className="border-x-2 border-y-2 border-slate-900 p-2 rounded-2xl" onClick={clickButton}>Click</button>
 
             <span className="ml-3 text-sm font-medium text-gray-900">Toggle Theme</span>
         </label>
